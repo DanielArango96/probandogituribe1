a67f293203f8b28a9167fe3b4236664975e43250
Esteban H <EstebanHernandez23201@hotmail.com>
2023-11-28 11:30:07 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Natillera la abuelita toretto
+        //¿Qué necesito para procesar la natillera de 1 cliente?
+        //ENTRADA
+        int identificadorCliente=233444;
+                long documentoIdentidad=1040751655908L;
+                byte edadCliente=34;
+                short cuotaMensualCliente=25000;
+                float tasaInteresFijo=0.1f;
+                char calificacionCliente='A';
+
+    }
+}
